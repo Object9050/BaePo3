b0eb890fae2313784e90e80d1cd06afc23a61013
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-05 17:06:31 +0200

    Next.js in docker compose auskommentiert

---
+++ my-baepi/nextjs/pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+ 
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body
+      className='bg-gradient-to-r
+      from-orange-300
+      via-yellow-300
+      to-white-400'
+      >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
