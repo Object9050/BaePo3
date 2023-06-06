18705f864c9f2333610f329e41c320e2e8c05ef5
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-06 11:02:56 +0200

    message_underline_removed

--- my-baepi/nextjs/pages/index.js
+++ my-baepi/nextjs/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <h1 className="font-bold text-5xl underline">BaePo is alive!</h1>
+      <h1 className="font-bold text-5xl">BaePo is alive!</h1>
     </Layout>
   );
   }
