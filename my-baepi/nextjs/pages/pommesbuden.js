6b065908d25f20ff76a563f5abefa84dd72c3dff
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-07 12:34:48 +0200

    Pagination w/ SWR

--- my-baepi/nextjs/pages/pommesbuden.js
+++ my-baepi/nextjs/pages/pommesbuden.js
@@ -1,18 +1,25 @@
 import { fetcher } from "../../lib/api";
 import Layout from "../components/Layout";
 import Pommesbuden from "../components/Pommesbuden";
+import useSWR from 'swr';
 
 const PommesList = ({ pommes }) => {
-  return (
-    <Layout>
-      <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4 mt-4">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 py-2">
-          Pommesbuden!
-        </span>
-      </h1>
-      <Pommesbuden pommes={pommes} />
-    </Layout>
+  const { data } = useSWR(`${process.env.NEXT_PUBLIC_STRAPI_URL}/pommesbuden`, 
+    fetcher, 
+    {
+      fallbackData: pommes
+    }
   );
+    return (
+      <Layout>
+      <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4 mt-4">
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 py-2">
+          Pommesbuden!
+          </span>
+      </h1>
+      <Pommesbuden pommes={data} />
+      </Layout>
+    );
 };
 
 export default PommesList;
