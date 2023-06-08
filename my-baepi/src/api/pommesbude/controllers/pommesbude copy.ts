4f66502a7076da47eb39af86214e599e6be3b8ab
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-08 14:45:12 +0200

    modified controller

---
+++ my-baepi/src/api/pommesbude/controllers/pommesbude copy.ts
@@ -0,0 +1,7 @@
+/**
+ * pommesbude controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::pommesbude.pommesbude');
--- my-baepi/src/api/pommesbude/controllers/pommesbude.ts
+++ my-baepi/src/api/pommesbude/controllers/pommesbude.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::pommesbude.pommesbude');
+export default factories.createCoreController('api::pommesbude.pommesbude', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.db.query('api::pommesbude.pommesbude').findOne({
+      where: { slug: id }
+    });
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+    return this.transformResponse(sanitizedEntity);
+  }
+}));
