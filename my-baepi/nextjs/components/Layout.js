ef7bb250099b54e7bc3ae85da692c55a7d9df41e
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-10 13:48:02 +0200

    edited layout

--- my-baepi/nextjs/components/Layout.js
+++ my-baepi/nextjs/components/Layout.js
@@ -1,15 +1,34 @@
 import React from "react";
+import Head from "next/head";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
-const Layout = ({ children }) => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <Nav />
-      <main className="flex-grow container mx-auto px-4">{children}</main>
-      <Footer />
-    </div>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <Head>
+      <title>BaePo - Best Pommes in Town!</title>
+    </Head>
+
+    <Nav />
+    <main className="flex-grow container mx-auto px-4">
+      <div
+        className="
+          flex
+          justify-center
+          items-center
+          bg-white
+          mx-auto
+          w-2/4
+          rounded-lg
+          my-16
+          p-16
+        "
+      >
+        <div className="text-2x font-medium">{children}</div>
+      </div>
+    </main>
+    <Footer />
+  </>
+);
 
 export default Layout;
