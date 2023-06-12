8378317a2bde90c3f0cfc9c74d041649d3c35dc3
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-12 12:57:24 +0200

    implemented UserContext on all pages

--- my-baepi/nextjs/pages/pommesbuden.js
+++ my-baepi/nextjs/pages/pommesbuden.js
@@ -3,8 +3,10 @@ import { fetcher } from "../lib/api";
 import Layout from "../components/Layout";
 import Pommesbuden from "../components/Pommesbuden";
 import useSWR from 'swr';
+import { useFetchUser } from "../lib/authContext";
 
 const PommesList = ({ pommes }) => {
+  const { user, loading } = useFetchUser();
 // Making pageIndex a variable through react's useState hook
 // setting the starting page to page 1
   const [pageIndex, setPageIndex] = useState(1);
@@ -16,7 +18,7 @@ const PommesList = ({ pommes }) => {
     }
   );
     return (
-      <Layout>
+      <Layout user={user}>
         <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4 mt-4">
           <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400 py-2">
           Pommesbuden!
