0e612bd8be0867dc1380443be8f19790e6437888
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-06-16 16:15:10 +0200

    code prettier

--- my-baepi/nextjs/pages/pommesbuden.js
+++ my-baepi/nextjs/pages/pommesbuden.js
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { fetcher } from "../lib/api";
 import Layout from "../components/Layout";
 import Pommesbuden from "../components/Pommesbuden";
-import useSWR from 'swr';
+import useSWR from "swr";
 import { useFetchUser } from "../lib/authContext";
 
 // Special Next.js function that pre-loads initial website data at build time.
-// Comes into effect as fallbackData in PommesList() and is shown before data 
+// Comes into effect as fallbackData in PommesList() and is shown before data
 // is requested by users.
 export async function getStaticProps() {
   const pommesResponse = await fetcher(
@@ -19,58 +19,57 @@ export async function getStaticProps() {
   };
 }
 
-// 
+//
 const PommesList = ({ pommes2 }) => {
   const { user, loading } = useFetchUser();
-// Making pageIndex a variable through react's useState hook
-// setting the starting page to page 1
+  // Making pageIndex a variable through react's useState hook
+  // setting the starting page to page 1
   const [pageIndex, setPageIndex] = useState(1);
   const { data } = useSWR(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/pommesbuden?pagination[page]=${pageIndex}&pagination[pageSize]=1`, 
-    fetcher, 
+    `${process.env.NEXT_PUBLIC_STRAPI_URL}/pommesbuden?pagination[page]=${pageIndex}&pagination[pageSize]=1`,
+    fetcher,
     {
       fallbackData: pommes2,
     }
   );
-    return (
-      <Layout user={user}>
-        <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4 mt-4">
-          <span className="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-400 py-2">
+  return (
+    <Layout user={user}>
+      <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter mb-4 mt-4">
+        <span className="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-yellow-400 py-2">
           Pommesbuden
-          </span>
-        </h1>
-        {/* Link to Pommesbuden component to show all Pommesbuden based on data fetched via useSWR */}
-        <Pommesbuden pommes={data} />
-        {/* Pagination Nav Buttons */}
-        <div className="space-x-2 space-y-2">
-          <button
-            className={`md:p-2 rounded py-2 text-black p-2 ${
-              pageIndex === 1 ? 'bg-gray-300 text-white' : 'bg-yellow-300'
-            }`}
-            disabled={pageIndex === 1}
-            onClick={() => setPageIndex(pageIndex -1)}
-          >
-            {' '}
-            Zurück
-          </button>
-          <button
-            className={`md:p-2 rounded py-2 text-black p-2 ${
-              pageIndex === (data && data.meta.pagination.pageCount)
-               ? 'bg-gray-300 text-white' : 'bg-yellow-300'
-            }`}
-            disabled={pageIndex === (data && data.meta.pagination.pageCount)}
-            onClick={() => setPageIndex(pageIndex +1)}
-          >
-            Weiter
-          </button>
-          <span>{`${pageIndex} von ${
-            data && data.meta.pagination.pageCount
+        </span>
+      </h1>
+      {/* Link to Pommesbuden component to show all Pommesbuden based on data fetched via useSWR */}
+      <Pommesbuden pommes={data} />
+      {/* Pagination Nav Buttons */}
+      <div className="space-x-2 space-y-2">
+        <button
+          className={`md:p-2 rounded py-2 text-black p-2 ${
+            pageIndex === 1 ? "bg-gray-300 text-white" : "bg-yellow-300"
           }`}
-          </span>
-        </div>
-      </Layout>
-    );
+          disabled={pageIndex === 1}
+          onClick={() => setPageIndex(pageIndex - 1)}
+        >
+          {" "}
+          Zurück
+        </button>
+        <button
+          className={`md:p-2 rounded py-2 text-black p-2 ${
+            pageIndex === (data && data.meta.pagination.pageCount)
+              ? "bg-gray-300 text-white"
+              : "bg-yellow-300"
+          }`}
+          disabled={pageIndex === (data && data.meta.pagination.pageCount)}
+          onClick={() => setPageIndex(pageIndex + 1)}
+        >
+          Weiter
+        </button>
+        <span>
+          {`${pageIndex} von ${data && data.meta.pagination.pageCount}`}
+        </span>
+      </div>
+    </Layout>
+  );
 };
 
 export default PommesList;
-
