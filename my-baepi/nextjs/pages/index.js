5e68f7375d84f9b360a0c2914f1f3a2a406c724f
Object9050 <111750980+Object9050@users.noreply.github.com>
2023-07-12 11:40:42 +0200

    Fully dockerized!
    Next.js also in a docker container now.

--- my-baepi/nextjs/pages/index.js
+++ my-baepi/nextjs/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Layout user={user}>
       <h1 className="font-bold text-2xl mb-4">BæPo - Bæst Pommes in Town!</h1>
-      <p className="text-xl mb-4">Abschlussprojekt von Jodie Sauer im Rahmen der DevOps-Weiterbildung bei Techstarter.</p>
+      <p className="text-xl mb-4">Herzlich Willkommen zum Abschlussprojekt von Jodie Sauer im Rahmen der DevOps-Weiterbildung bei Techstarter.</p>
       <p className="text-xl">Guten Appetit!</p>
     </Layout>
   );
